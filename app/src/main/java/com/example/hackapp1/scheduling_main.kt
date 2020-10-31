41f26e17354256d61e033ca1b3e5c4141a5c26b6
anirudh-dhoundiyal <anirudh_dhoundiyal@icloud.com>
2020-10-31 13:40:19 -0500

    third commit

--- app/src/main/java/com/example/hackapp1/scheduling_main.kt
+++ app/src/main/java/com/example/hackapp1/scheduling_main.kt
@@ -1,11 +1,41 @@
 package com.example.hackapp1
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.AutoTransition
+import android.transition.TransitionManager
+import android.view.View
+import androidx.annotation.RequiresApi
+import kotlinx.android.synthetic.main.activity_scheduling_main.*
 
 class scheduling_main : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scheduling_main)
+
+        upschedule_card.setOnClickListener {
+            if (expandableLayout.visibility == View.GONE) {
+                TransitionManager.beginDelayedTransition(upschedule_card,AutoTransition())
+                expandableLayout.visibility = View.VISIBLE
+            } else {
+                TransitionManager.beginDelayedTransition(upschedule_card, AutoTransition())
+                expandableLayout.visibility = View.GONE
+            }
+        }
+
+        uppast_card.setOnClickListener {
+            if (expandableLayout.visibility == View.GONE) {
+                TransitionManager.beginDelayedTransition(uppast_card,AutoTransition())
+                pastexpandableLayout.visibility = View.VISIBLE
+            } else {
+                TransitionManager.beginDelayedTransition(uppast_card, AutoTransition())
+                pastexpandableLayout.visibility = View.GONE
+            }
+        }
+
+
+
     }
 }
